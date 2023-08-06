4285f21495ebb097173e3051cb22f2bd2590f359
sharkey52 <jpr.sharkey@gmail.com>
2023-08-06 15:08:24 +0100

    added booking form

--- src/components/BookDive.js
+++ src/components/BookDive.js
@@ -1,15 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/componentstyles/BookDive.css';
 
 const DiveCenter = () => {
-    return (
-        <div className="buttonGroup">
-            <button className="dc-button">Book Again</button>
-            <button className="dc-button">Courses</button>
-            <button className="dc-button">Group Trips</button>
-            <button className="dc-button">Deals</button>
-        </div>
-    );
+  const [numDivers, setNumDivers] = useState(1);
+  const [selectedRegion, setSelectedRegion] = useState(null);
+  const [hasDisabilities, setHasDisabilities] = useState(false);
+
+  const handleRegionChange = (event) => {
+    setSelectedRegion(event.target.value);
+  };
+
+  const handleNumDiversChange = (event) => {
+    setNumDivers(Number(event.target.value));
+  };
+
+  const handleDisabilitiesChange = (event) => {
+    setHasDisabilities(event.target.checked);
+  };
+
+  return (
+    <div className="diveCenter">
+      <div className="buttonGroup">
+        <button className="dc-button">Book Again</button>
+        <button className="dc-button">Courses</button>
+        <button className="dc-button">Group Trips</button>
+        <button className="dc-button">Deals</button>
+      </div>
+      <div className="booking-form">
+        <form action="booking.php" method="post">
+          <label htmlFor="name">Your Name</label>
+          <input type="text" name="name" placeholder="Your name" />
+
+          <label htmlFor="email">Your Email</label>
+          <input type="email" name="email" placeholder="Your email" />
+
+          <label htmlFor="date">Date of Booking</label>
+          <input type="date" name="date" placeholder="Date of booking" />
+
+          <label htmlFor="numDivers">Number of Divers</label>
+          <select name="numDivers" id="numDivers" onChange={handleNumDiversChange}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            {/* Add more options if needed */}
+          </select>
+
+          {numDivers <= 4 && (
+            <div>
+              {[...Array(numDivers)].map((_, index) => (
+                <div key={index}>
+                  <label htmlFor={`certification_level_${index + 1}`}>Diver {index + 1} Certification Level</label>
+                  <select name={`certification_level_${index + 1}`} id={`certification_level_${index + 1}`}>
+                    <option value="beginner">Beginner</option>
+                    <option value="certified">Certified</option>
+                    <option value="professional">Professional</option>
+                  </select>
+                </div>
+              ))}
+            </div>
+          )}
+
+          {numDivers <= 4 && (
+            <div>
+              <label htmlFor="disabilities">Any Disabilities in the Group?</label>
+              <input type="checkbox" name="disabilities" id="disabilities" onChange={handleDisabilitiesChange} />
+            </div>
+          )}
+
+          {!selectedRegion && numDivers <= 4 && (
+            <div>
+              <label htmlFor="region">Region</label>
+              <select name="region" id="region" onChange={handleRegionChange}>
+                <option value="">Select a region</option>
+                <option value="south-america">South America</option>
+                <option value="caribbean">Caribbean</option>
+                <option value="west-coast-usa">West Coast USA</option>
+                {/* Add more options for other regions */}
+              </select>
+            </div>
+          )}
+
+          {selectedRegion && (
+            <div className="region-box">
+              {/* Red rectangle or region-specific content */}
+            </div>
+          )}
+
+          {numDivers <= 4 && (
+            <input type="submit" value="Book Now" className="submit-button" />
+          )}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default DiveCenter;
--- src/components/DiveLogBook.js
+++ src/components/DiveLogBook.js
@@ -6,78 +6,84 @@ import LoadingScreen from '../LoadingScreen';
 import '../styles/componentstyles/DiveLogBook.css';
 
 const DiveLogBook = () => {
-const [dives, setDives] = useState([]);
-const [loading, setLoading] = useState(true);
-const [isFormOpen, setFormOpen] = useState(false);
+  const [dives, setDives] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isFormOpen, setFormOpen] = useState(false);
 
-const fetchDives = async () => {
-  setLoading(true);
+  const fetchDives = async () => {
+    setLoading(true);
 
-  try {
-    const response = await fetch(
-      'https://i14lwecdl6.execute-api.eu-west-2.amazonaws.com/test',
-	    {
-		    method: 'PUT',
-		    body: JSON.stringify({"httpMethod":"GET"})}
-    );
-    console.log(response)
-    if (response.ok) {
+    try {
+      const response = await fetch(
+        'https://tzwxxjtn58.execute-api.eu-west-2.amazonaws.com/prod',
+        {
+          method: 'GET'
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+
       const data = await response.json();
-      setDives(JSON.parse(data.body)); // Parse the response body
+      setDives(data);
+    } catch (error) {
+      console.error('Error fetching dives:', error);
+    } finally {
+      setLoading(false);
     }
-  } catch (error) {
-    console.error('Error fetching dives:', error);
-  } finally {
-    setLoading(false);
-  }
-};
+  };
 
+  useEffect(() => {
+    fetchDives();
+  }, []);
 
-useEffect(() => {
-fetchDives();
-}, []);
+  const fetchMoreData = () => {
+    // Implement this function to fetch more data and append it to the current dives state.
+    // You might need a pagination mechanism on the server-side to fetch additional data.
+    // For the sake of simplicity, I'll assume this function will not be called in this example.
+  };
 
-const fetchMoreData = () => {};
+  const handleOpenForm = () => {
+    setFormOpen(true);
+  };
 
-const handleOpenForm = () => {
-setFormOpen(true);
-};
+  const handleCloseForm = () => {
+    setFormOpen(false);
+    fetchDives();
+  };
 
-const handleCloseForm = () => {
-setFormOpen(false);
-fetchDives();
-};
-
-return (
-<div className="diveLogBook">
-<button className="addDiveButton" onClick={handleOpenForm}>
-+
-</button>
-<Modal
-     isOpen={isFormOpen}
-     onRequestClose={handleCloseForm}
-     contentLabel="Dive Log Form"
-   >
-<DiveLogForm />
-</Modal>
-{loading ? (
-<LoadingScreen />
-) : (
-<InfiniteScroll
-dataLength={dives.length}
-next={fetchMoreData}
-hasMore={true}
-loader={<h4>Loading...</h4>}
->
-{dives.map((dive, index) => (
-<div className="diveitem" key={index}>
-{dive.date}, {dive.time}, {dive.location}, {dive.depth}, {dive.depth}, {dive.length}, {dive.temperature}, {dive.company}, {dive.weather}, {dive.water_temperature}
-</div>
-))}
-</InfiniteScroll>
-)}
-</div>
-);
+  return (
+    <div className="diveLogBook">
+      <button className="addDiveButton" onClick={handleOpenForm}>
+        +
+      </button>
+      <Modal
+        isOpen={isFormOpen}
+        onRequestClose={handleCloseForm}
+        contentLabel="Dive Log Form"
+      >
+        <DiveLogForm />
+      </Modal>
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <InfiniteScroll
+          dataLength={dives.length}
+          next={fetchMoreData}
+          hasMore={false} // Set this to true if there's more data to fetch
+          loader={<h4>Loading...</h4>}
+        >
+          {dives.map((dive, index) => (
+            <div className="diveitem" key={index}>
+              {dive.date}, {dive.time}, {dive.location}, {dive.depth}, {dive.length}, {dive.temperature}, {dive.company}, {dive.weather}, {dive.water_temperature}
+            </div>
+          ))}
+        </InfiniteScroll>
+      )}
+    </div>
+  );
 };
 
 export default DiveLogBook;
+
